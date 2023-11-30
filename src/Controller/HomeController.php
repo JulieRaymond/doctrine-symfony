6de79bbd7cf1820b7a49cfe8e2571b01704236ac
Nicolas Leroux <hertford.fr@gmail.com>
2023-11-30 16:09:02 +0100

    add nuts'

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\NutRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,15 @@ use App\Repository\SquirrelRepository;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(SquirrelRepository $squirrelRepository): Response
+    public function index(SquirrelRepository $squirrelRepository,
+                            NutRepository $nutRepository): Response
     {
         $squirrels = $squirrelRepository->findAll();
+        $nuts = $nutRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'squirrels' => $squirrels,
-        ]);    }
+            'nuts' => $nuts,
+        ]);
+    }
 }
